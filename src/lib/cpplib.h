c3676bf7b9bfd0b0ef445b4751e3117e6e109a0a
WenhaoCui <59945706+Wenhaocui@users.noreply.github.com>
2021-01-31 19:00:58 -0800

    Update cpplib.h

--- src/lib/cpplib.h
+++ src/lib/cpplib.h
@@ -30,7 +30,7 @@ class CPPLib {
   bool canBePalindrome(const std::string &str);
 
   //q7
-  std::map<char, char> Mappable(const std::string& from, const std::string& to);
+  std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs);
 
   //q8
   void kthPeek(std::vector<int> &input, int k);
